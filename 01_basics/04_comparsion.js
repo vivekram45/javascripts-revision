b143691ab382c0a178b2c223f9a7e0f56dbcf281
Vivek Ram <158698702+vivekram45@users.noreply.github.com>
2026-08-04 10:22:10 +0000

    comparsion

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,9 @@
+// console.log( 2 > 1) // true
+// console.log( 2 < 1) // false
+// console.log( 2 == 1) // false
+// console.log( 2 != 1) // true
+// console.log( 2 >= 1) // true
+// console.log( 2 <= 1) // false
+
+console.log( "2" > 1); // true
+console.log("02" > 1);  
