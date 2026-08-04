2dfad131657be5b8014781bf77ebf8efa7f6ef46
Vivek Ram <158698702+vivekram45@users.noreply.github.com>
2026-08-04 07:40:17 +0000

    container

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountemail = "vivekram4005@gmail.com"
+var accountPassword = "12345"
+accountcity = "Jaipur"
+let accountState;
+
+// const is also fixed it cannot be a change 
+
+accountemail = "vram45@gmail.com"
+accountPassword="56458"
+accountcity= "bihar"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountemail,accountPassword,accountcity,accountstate])
