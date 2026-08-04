35a6a37cfc9f27845bdd25b9f1c3e09f6bf0f1c1
Vivek Ram <158698702+vivekram45@users.noreply.github.com>
2026-08-04 10:03:00 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,38 +1,87 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score) //string
-console.log(typeof (score)); //string
+// console.log(typeof score) //string
+// console.log(typeof (score)); //string
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber) //number
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) //number
 
-let book = "450"
-let bookInNumber = Number(book)
-console.log(typeof bookInNumber) //number
+// let book = "450"
+// let bookInNumber = Number(book)
+// console.log(typeof bookInNumber) //number
 
- let study = 45
- let studyInString = String(study)
- console.log(typeof studyInString) //string
+//  let study = 45
+//  let studyInString = String(study)
+//  console.log(typeof studyInString) //string
 
- let pen = "34abc"
- let penInNumber = Number(pen)
-    console.log(penInNumber) //NaN
-    console.log(typeof penInNumber) //number
+//  let pen = "34abc"
+//  let penInNumber = Number(pen)
+//     console.log(penInNumber) //NaN
+//     console.log(typeof penInNumber) //number
 
-/*Q1. Why does penInNumber give NaN?
+// /*Q1. Why does penInNumber give NaN?
 
-Number("34abc") tries to convert the entire string into a number. Since "34abc" contains letters (abc), it is not a valid number. Therefore, JavaScript returns NaN (Not-a-Number), which means the conversion failed. */
+// Number("34abc") tries to convert the entire string into a number. Since "34abc" contains letters (abc), it is not a valid number. Therefore, JavaScript returns NaN (Not-a-Number), which means the conversion failed. */
+
+// /*
+//   Q2. Why does typeof penInNumber give "number"?
+
+// NaN is a special value that belongs to JavaScript's Number data type. Even though it means "Not-a-Number," JavaScript still treats it as a numeric value. Therefore, typeof NaN returns "number".
+// */
+
+// //"33" => NAN 
+// // "33abc" => NAN
+// // true => 1  ;  false => 0
+
+// let loggedIn = "Vivek"
+// let loggedInInNumber = Boolean(loggedIn)
+// console.log(loggedInInNumber) //true
+
+
+
+//******************operations **************** //
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue) // -3
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2) //modulus operator
+// console.log(2**3) //exponentiation operator
+
+// let str1 = "Hello"
+// let str2 = "World"
+// console.log(str1 + str2) //HelloWorld
+// console.log(str1 + " " + str2) //Hello World
+
+// console.log(2 + "2") //22
+// console.log("2" + 2) //22
+// console.log("2" + 2 + 2) //222
+// console.log(2 + 2 + "2") //42
+// console.log(2 - "2") //0
+// console.log("2" - 2) //0
+// console.log(2 * "2") //4
+// console.log("2" * 2) //4
+// console.log(2 / "2") //1
+// console.log("2" / 2) //1
+// console.log(2 % "2") //0
+// console.log("2" % 2) //0
+// console.log(2 ** "2") //4
+// console.log("2" ** 2) //4
+// console.log(2 + "2" + 2) //222
+
+console.log(+true);
+console.log(+"");
 
 /*
-  Q2. Why does typeof penInNumber give "number"?
-
-NaN is a special value that belongs to JavaScript's Number data type. Even though it means "Not-a-Number," JavaScript still treats it as a numeric value. Therefore, typeof NaN returns "number".
+  let x = 5
+  Type      Meaning                  Example Output   final 
+  prefix    Change first,use later    ++x      6      6
+  postfix   Use first,change later    x++      5      6
+  prefix    Change first, use later   --x      4      4
+  postfix   Use first, change later   x--      5      4
 */
 
-//"33" => NAN 
-// "33abc" => NAN
-// true => 1  ;  false => 0
-
-let loggedIn = "Vivek"
-let loggedInInNumber = Boolean(loggedIn)
-console.log(loggedInInNumber) //true
