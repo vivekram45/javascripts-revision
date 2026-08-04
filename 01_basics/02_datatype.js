4f23ea92be52f38582e4052695b2573995ff3da6
Vivek Ram <158698702+vivekram45@users.noreply.github.com>
2026-08-04 08:02:42 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+let name = "vivek" // string data type
+let age = 45 // number data type 
+let loggedin = false  // boolean data type 
+
+
+// number => 2 to power 53 
+// bigint
+
+//string =>""
+//boolean => true/false
+//null => stand alone value //empty //object hai (output) type hai
+//underfined => // value sign nahi h // type hai undefined 
+//symbol => unique
+
+
+//object
